918ae849598436af4fca905fdb228f16041d3e7a
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-05 14:32:02 +0500

    Added Variables

---
+++ variables/variable.js
@@ -0,0 +1,17 @@
+var info = "Student Info"
+var name = "Ali"
+var age = 19
+var feePaid = true
+var nationality = "Pakistan"
+var course
+course = "CNC"
+
+alert(info)
+console.log(name,age,nationality,course,feePaid)
+document.write(
+    "Name : ",name,"<br/>",
+    "Age : ",age,"<br/>",
+    "Nationality : ",nationality,"<br/>",
+    "Course : ",course,"<br/>",
+    "Fee Paid : ",feePaid
+    )
