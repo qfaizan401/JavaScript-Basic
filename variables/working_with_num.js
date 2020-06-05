b9f8cd9dae7d0501be943981f2a0deb6a83ed718
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-05 15:26:53 +0500

    Added Numbers var

---
+++ variables/working_with_num.js
@@ -0,0 +1,21 @@
+var num1 = 5
+var num2 = 7
+
+var add = num1+num1
+var sub = num1-num2
+var mul = num1*num2
+var div = num1/num2
+
+console.log(add)
+console.log(sub)
+console.log(mul)
+console.log(div)
+
+console.log(
+    add,"<br/>",
+    sub,"<br/>",
+    mul,"<br/>",
+    div
+)
+
+alert(add+'\n'+sub+'\n'+mul+'\n'+div)
