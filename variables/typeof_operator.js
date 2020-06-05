e70853a5a17b08958cb92a9840d9f367920b35f2
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-05 16:30:08 +0500

    Till typeof operator

---
+++ variables/typeof_operator.js
@@ -0,0 +1,24 @@
+var a = 15
+console.log(a)
+console.log(typeof a)
+
+
+a = "John"
+console.log(a)
+console.log(typeof a)
+
+a = true
+console.log(a)
+console.log(typeof a)
+
+a = 1.5
+console.log(a)
+console.log(typeof a)
+
+a = undefined
+console.log(a)
+console.log(typeof a)
+
+a = null
+console.log(a)
+console.log(typeof a)
