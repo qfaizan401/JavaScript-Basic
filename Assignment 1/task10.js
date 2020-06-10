b4a07d1854d5f1b74d4b9a310867c340613327b2
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-11 00:23:14 +0500

    added task 10

---
+++ Assignment 1/task10.js
@@ -0,0 +1,20 @@
+/*
+Do the following using JS Mathematic Expressions
+a. Declare a variable.
+b. Show the value of variable in your browser like “Value after
+variable declaration is: ??”.
+c. Initialize the variable with some number.
+d. Show the value of variable in your browser like “Initial value: 5”.
+e. Increment the variable.
+f. Show the value of variable in your browser like “Value after
+increment is: 6”.
+g. Add 7 to the variable.
+h. Show the value of variable in your browser like “Value after
+addition is: 13”.
+i. Decrement the variable.
+j. Show the value of variable in your browser like “Value after
+decrement is: 12”.
+k. Show the remainder after dividing the variable’s value by 3.
+l. Output : “The remainder is : 0”.
+*/
+
---
+++ Assignment 1/task8.js
@@ -0,0 +1,6 @@
+//Write a program that takes two numbers & add them in a new variable. Show the result in your browser.
+
+var a = 5; b = 7
+
+result = a+b
+document.write("Sum of ",a," and ",b," is ",result)
