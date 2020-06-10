8325839c37258ad89930e740528c85748a98e916
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-10 22:56:51 +0500

    Added task 4

---
+++ Assignment 1/task3.js
@@ -0,0 +1,6 @@
+/*Declare a variable called email and assign to it a string that represents your 
+Email Address(e.g. example@example.com). Show the below mentioned message in an 
+alert box.(Hint: use string concatenation)*/
+
+var email = "qfaizan401@gmail.com"
+alert("My Email Address is "+email)
---
+++ Assignment 1/task4.js
@@ -0,0 +1,3 @@
+//Write a script to display this in browser through JS
+
+document.write("Yeah!!! I can write HTML content through JS")
