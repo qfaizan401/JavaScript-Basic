2acb3caecec7110a86b4c5914fd3b9e8dfe4c7c4
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-10 22:48:52 +0500

    Update task2.js

--- Assignment 1/task2.js
+++ Assignment 1/task2.js
@@ -1,3 +1,6 @@
 //Write a script to save student’s bio data in JS variables and show the data in alert boxes.
-var firstName = "Jhone"; lastName = "Doe"
+var firstName = "Jhone"; lastName = "Doe"; age = 15; course = "Certified Mobile App Developer"
+
 alert(firstName+" "+lastName)
+alert(15+" Years old")
+alert(course)
