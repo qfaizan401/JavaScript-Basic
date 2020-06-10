9639b0c49bce1ba33c3fc96ba9d3048d91a60605
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-10 23:09:20 +0500

    added task 6

---
+++ Assignment 1/task6.js
@@ -0,0 +1,9 @@
+//Declare a variable called birthYear & assign to it your birth year. 
+//Show the following message in your browser:
+
+var birthYear = 1999
+
+document.write(
+    "My brith year is ", birthYear, "<br/>",
+    "Datatype of my declared variable is: ", typeof(birthYear)
+)
