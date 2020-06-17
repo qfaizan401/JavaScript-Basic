b3b5b46870d04db70690fa2ce84986fc74b66855
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-18 00:56:23 +0500

    Update if_else.js

--- one step ahead/if_else.js
+++ one step ahead/if_else.js
@@ -4,7 +4,14 @@ if (age == 0) {
     alert("You did not answer :)")
 }
 else if (age >= 18) {
-    alert("Can you the PARTY!!!")
+    alert("You can go to the PARTY!!! but wait...")
+    var license = prompt("Do you have a License?")
+    if (license == "yes") {
+        alert("Thats great WELCOME :)")
+    }
+    else {
+        alert("Pls get the License")
+    }
 }
 else {
     alert("SORRY :(")
