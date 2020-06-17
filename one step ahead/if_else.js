520321eef4175ad64a9d48796547b3e5fcefaf8b
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-18 00:44:52 +0500

    if_else added

---
+++ one step ahead/if_else.js
@@ -0,0 +1,11 @@
+var age = Number(prompt("What is you age?"))
+
+if (age == 0) {
+    alert("You did not answer :)")
+}
+else if (age >= 18) {
+    alert("Can you the PARTY!!!")
+}
+else {
+    alert("SORRY :(")
+}
