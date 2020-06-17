fd3dbe0e3b6da6edbc89bd71a0ec94922504b07e
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-18 00:23:47 +0500

    comparison

---
+++ one step ahead/comparison_operator.js
@@ -0,0 +1,28 @@
+/*
+Commom operators
+==
+!=
+<
+>
+<=
+>=
+JS Unique oparators (Check data type also)
+===
+!==
+*/
+
+var a = 7
+var b = 2
+console.log(a == b)
+console.log(a != b)
+console.log(a < b)
+console.log(a > b)
+console.log(a <= b)
+console.log(a >= b)
+console.log(" === & !==")
+
+var x = 5
+var y = "5"
+console.log(x == y)
+console.log(x === y)
+console.log(x !== y)
